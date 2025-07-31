401b988728b31316b8645dfd3b0e0f32fca4ea78
Otto Zihor <zihorotto@gmail.com>
2025-07-31 10:09:47 +0200

    V2 - 12

--- frontend/src/plugins/cognitoOidc.ts
+++ frontend/src/plugins/cognitoOidc.ts
@@ -1,28 +1,31 @@
 import { UserManager } from 'oidc-client-ts'
 import { useRuntimeConfig } from '#app'
 
-// Config beolvasása Nuxt runtimeConfig-ból
-const config = useRuntimeConfig()
+export function createUserManager() {
+  const config = useRuntimeConfig()
 
-const cognitoAuthConfig = {
-  authority: String(config.public.NUXT_PUBLIC_COGNITO_AUTHORITY),
-  client_id: String(config.public.NUXT_PUBLIC_COGNITO_CLIENT_ID),
-  redirect_uri: String(config.public.NUXT_PUBLIC_COGNITO_REDIRECT_URI),
-  response_type: 'code',
-  scope: 'phone openid email'
+  const cognitoAuthConfig = {
+    authority: String(config.public.NUXT_PUBLIC_COGNITO_AUTHORITY),
+    client_id: String(config.public.NUXT_PUBLIC_COGNITO_CLIENT_ID),
+    redirect_uri: String(config.public.NUXT_PUBLIC_COGNITO_REDIRECT_URI),
+    response_type: 'code',
+    scope: 'phone openid email'
+  }
+
+  return new UserManager({
+    ...cognitoAuthConfig
+  })
 }
 
-// UserManager példány
-export const userManager = new UserManager({
-  ...cognitoAuthConfig
-})
-
-export async function signOutRedirect () {
+export async function signOutRedirect() {
+  const userManager = createUserManager()
   await userManager.removeUser()
 
+  const config = useRuntimeConfig()
   const clientId = config.public.NUXT_PUBLIC_COGNITO_CLIENT_ID
   const logoutUri = config.public.NUXT_PUBLIC_COGNITO_LOGOUT_URI
   const cognitoDomain = config.public.NUXT_PUBLIC_COGNITO_DOMAIN
+
   if (import.meta.client) {
     // eslint-disable-next-line no-undef
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(
@@ -30,4 +33,3 @@ export async function signOutRedirect () {
     )}`
   }
 }
-
